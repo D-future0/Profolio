c37a04ea04aa517a7cf25071b39bbcf5df556cc9
D-future0 <oka4collins@gmail.com>
2024-04-06 14:53:50 -0700

    style update

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const hamburgerMenu = document.querySelector('.hamburger-icon');
 const menuLinks = document.querySelector('.menu-links');
-const downloadResume = document.querySelector('.download-resume');
-const contact = document.querySelector('#contact-info');
+
 
 hamburgerMenu.addEventListener(`click`, ()=>{
     menuLinks.classList.toggle('open-menu-links')
 });
 
-downloadResume.addEventListener(`click`, ()=>{
-    window.open("./myCV.JPG")
-})
 
